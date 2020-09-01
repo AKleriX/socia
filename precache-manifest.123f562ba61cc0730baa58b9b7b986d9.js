d630504566ec73c057b96e16e2bc3e5509edc5e0
AKleriX <AKleriX@users.noreply.github.com>
2020-09-01 23:37:37 +0300

    Updates

---
+++ precache-manifest.123f562ba61cc0730baa58b9b7b986d9.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ddc56a105e75432b08b69c0691f922",
+    "url": "/socia/index.html"
+  },
+  {
+    "revision": "054425b93306f8da4281",
+    "url": "/socia/static/css/main.15881f8a.chunk.css"
+  },
+  {
+    "revision": "b9fdcfc4774dfdc1e95e",
+    "url": "/socia/static/js/2.a4eeebc3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/socia/static/js/2.a4eeebc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "054425b93306f8da4281",
+    "url": "/socia/static/js/main.d7735402.chunk.js"
+  },
+  {
+    "revision": "6c8b7b7079bcada2b6a0",
+    "url": "/socia/static/js/runtime-main.615cd3e3.js"
+  },
+  {
+    "revision": "0ab9ee9757e3a196960d3239e95da0b8",
+    "url": "/socia/static/media/aboutMe.0ab9ee97.svg"
+  },
+  {
+    "revision": "ead9e08b60031bd03f496d50ba72608e",
+    "url": "/socia/static/media/camera.ead9e08b.png"
+  },
+  {
+    "revision": "add3a74676ded06ae620f8cc710ae394",
+    "url": "/socia/static/media/contact.add3a746.svg"
+  },
+  {
+    "revision": "d9f433b042eda9ab9f56fda249e11bec",
+    "url": "/socia/static/media/defaultUser.d9f433b0.png"
+  },
+  {
+    "revision": "f067b70eab8c70e73e81f89f73262dae",
+    "url": "/socia/static/media/edit.f067b70e.svg"
+  },
+  {
+    "revision": "2febff40f68867f39c020b96812a25f1",
+    "url": "/socia/static/media/login.2febff40.svg"
+  },
+  {
+    "revision": "d1465b428f941e57079c5e12d29639b2",
+    "url": "/socia/static/media/loginPreloader.d1465b42.svg"
+  },
+  {
+    "revision": "50f8d06970c61c07391825b82327122a",
+    "url": "/socia/static/media/logout.50f8d069.svg"
+  },
+  {
+    "revision": "27a9f79df272597d26c162a65c0bd85e",
+    "url": "/socia/static/media/pagesPreloader.27a9f79d.svg"
+  },
+  {
+    "revision": "a99213dfe7d30d75964edfd2ede4750f",
+    "url": "/socia/static/media/search.a99213df.svg"
+  },
+  {
+    "revision": "af2912acee74a45a37ec36b10a8dd2fd",
+    "url": "/socia/static/media/searchIcon.af2912ac.svg"
+  },
+  {
+    "revision": "ec2d71a7b2405482f625f2a6409501a0",
+    "url": "/socia/static/media/skills.ec2d71a7.svg"
+  },
+  {
+    "revision": "0dd2b8ba27ad5029e66f8f183cfa3b44",
+    "url": "/socia/static/media/sociaLogoName.0dd2b8ba.png"
+  },
+  {
+    "revision": "807ca40d7c32843c7a624d76c34a7b23",
+    "url": "/socia/static/media/user.807ca40d.svg"
+  }
+]);
